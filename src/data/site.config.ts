09159bb2f8d2f8b3d25c0ebcc514cef113fa1b30
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-09-20 23:14:37 +0300

    feat: Update site configuration with new author and title, adjust read time calculation

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,11 +9,11 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	author: 'Mohamed Fazrin', // Site author
+	title: 'Dotnet Evangelist | Mohamed Fazrin', // Site title.
+	description: 'Sharing my knowledge and experiences with the .NET community.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
-	shareMessage: 'Share this post', // Message to share a post on social media
-	paginationSize: 6 // Number of posts per page
+	shareMessage: 'Share', // Message to share a post on social media
+	paginationSize: 9 // Number of posts per page
 }
--- src/utils/readTime.ts
+++ src/utils/readTime.ts
@@ -1,9 +1,6 @@
 import getReadingTime from 'reading-time'
 import { toString } from 'mdast-util-to-string'
 
-/**
- * Injects `minutesRead` into frontmatter processed by Remark.
- */
 export function remarkReadingTime() {
 	return function (tree: unknown, { data }: any) {
 		const textOnPage = toString(tree)
