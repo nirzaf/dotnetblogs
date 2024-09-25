82ca2071e8cdbd34d6a1aded0db0743ce48d4aa5
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-09-25 22:38:27 +0300

    Updates site URL and base path for GitHub Pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,5 +31,11 @@ export default defineConfig({
 		}),
 		sitemap(),
 		tailwind()
-	]
+	],
+	vite: {
+		optimizeDeps: {
+			noDiscovery: true,
+			include: [], // Or leave as undefined
+		},
+	},
 })
