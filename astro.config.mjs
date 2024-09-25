86057032c1a2215591ec5d5f0ff2716227a9eab5
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-09-25 22:29:53 +0300

    fix(astro.config.mjs): Correct site configuration for GitHub Pages subfolder

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import { remarkReadingTime } from '@/utils'
+import { remarkReadingTime } from './src/utils/readTime'
 
 // https://astro.build/config
 export default defineConfig({
-	// Correct site configuration for GitHub Pages subfolder
-	site: 'https://nirzaf.github.io/',
+	// Write here your website url
+	site: 'https://nirzaf.github.io/dotnetblogs/', 
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
 		drafts: true,
