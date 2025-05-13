45bea66c59253871633a22e7158b695782ad724f
nirzaf <mfmfazrin1986@gmail.com>
2025-05-13 23:39:32 +0300

    feat: implement blog pagination and rename navigation menu item to Blogs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,7 +69,7 @@ export default function RootLayout({
                         href="/blog"
                         className="text-gray-800 dark:text-gray-200 font-medium hover:text-blue-600 dark:hover:text-blue-400 py-2 transition-colors relative group"
                       >
-                        Blog
+                        Blogs
                         <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-600 dark:bg-blue-400 group-hover:w-full transition-all duration-300"></span>
                       </Link>
                     </li>
